d080521f6501b6ad80ac7bbc47fb739bd02305a0
DeoluVS <deoluv.s@icloud.com>
2025-10-21 18:27:28 +0100

    Small error in redirector.js file redirect is working

--- assets/scripts/redirector.js
+++ assets/scripts/redirector.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     //Deployment: "/q2-coursework-blueprint/index.html" Testing: /index.html
     //Using ternary statement to check for a path that works for both phases
     //If true it adds on q2-coursework-blueprint and if not then it assigns "" or an empty string to basePath
-    const basePath = window.pathname.includes("/q2-coursework-blueprint")
+    const basePath = window.location.pathname.includes("/q2-coursework-blueprint")
     ? "/q2-courswork-blueprint" : "";
 
     const VALID_PATHS = [
@@ -19,14 +19,14 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     const isKnown = VALID_PATHS.includes(currentPath);
     if (!isKnown){
-        window.location.replace("/index.html");
+        window.location.replace(`${basePath}/index.html`);
     }
     //Added a redirect for any levels not completed fully. Normally activated after 5 seconds when coming soon is opened
-    if(currentPath == "/coming-soon.html"){
+    if(currentPath == `${basePath}/coming-soon.html`){
         const t = setTimeout(() => {console.log("Redirect message up");
                 }, 1000);
         document.getElementById("redirectMessage").textContent = "Redirecting to level Select";
-        const u = setTimeout(() => {window.location.replace("/level-select.html");
+        const u = setTimeout(() => {window.location.replace(`${basePath}/level-select.html`);
                 }, 5000);
     }
 
